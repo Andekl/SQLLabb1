8ef512f5099b754e927fcab1564bfb3b69eb0339
johannaastrom <johannaastrom@live.se>
2017-11-20 20:38:39 +0100

    Testing testing.

--- SQLLabb1/MainWindow.xaml.cs
+++ SQLLabb1/MainWindow.xaml.cs
@@ -49,62 +49,62 @@ namespace SQLLabb1
                             a.Nationality = reader.GetString(2);
                             
                             AuthorListBox.Items.Add(a.Name);
+                            
                         }
                     }
                 }
             }
 
 
-            string connectionString1 = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Library;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-            using (SqlConnection con1 = new SqlConnection(connectionString1))
-            {
-                con1.Open();
-                var query = "SELECT Title FROM Book";
-                using (SqlCommand cmd = new SqlCommand(query, con1))
-                {
-                    using (SqlDataReader reader1 = cmd.ExecuteReader())
-                    {
-                        while (reader1.Read())
-                        {
-                            Book b = new Book();
-                            b.BookId = reader1.GetInt32(0);
-                            b.AuthorID = reader1.GetInt32(1);
-                            b.Title = reader1.GetString(2);
-
-                            BookListBox.Items.Add(b.Title);
-                        }
-                    }
-                }
-            }
-        }
-
-        private void AuthorListBox_SelectionChanged(object sender, SelectionChangedEventArgs e) //Gör samma för BookListBox
-        {
-            //string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Library;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-            //using (SqlConnection con = new SqlConnection(connectionString))
+            //string connectionString1 = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Library;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            //using (SqlConnection con1 = new SqlConnection(connectionString1))
             //{
-            //    con.Open();
-            //    var query = "SELECT * FROM Author";
-            //    using (SqlCommand cmd = new SqlCommand(query, con))
+            //    con1.Open();
+            //    var query = "SELECT Title FROM Book";
+            //    using (SqlCommand cmd = new SqlCommand(query, con1))
             //    {
-            //        using (SqlDataReader reader = cmd.ExecuteReader())
+            //        using (SqlDataReader reader1 = cmd.ExecuteReader())
             //        {
-            //            while (reader.Read())
+            //            while (reader1.Read())
             //            {
-            //                column = new Dictionary<string, string>();
-            //                column["Nationality"] = reader["Nationality"].ToString();
-            //                column["Id"] = reader["Id"].ToString();
-            //                column["Name"] = reader["Name"].ToString();
+            //                Book b = new Book();
+            //                b.BookId = reader1.GetInt32(0);
+            //                b.AuthorID = reader1.GetInt32(1);
+            //                b.Title = reader1.GetString(2);
 
-            //                Dispatcher.Invoke(() =>
-            //                {
-            //                    AuthorTextBox.Text = reader.GetString(0);
-            //                });
-
+            //                BookListBox.Items.Add(b.Title);
             //            }
             //        }
             //    }
             //}
+        }
+
+        private void AuthorListBox_SelectionChanged(object sender, SelectionChangedEventArgs e) //Gör samma för BookListBox
+        {
+            var aa = sender as ListBox;
+            string author = aa.SelectedItem.ToString();
+            
+            string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Library;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                var query = "SELECT * FROM Author where Name = '" + author + "'";
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                                Author a = new Author();
+                                a.Id = reader.GetInt32(0);
+                                a.Name = reader.GetString(1);
+                                a.Nationality = reader.GetString(2);
+
+                            AuthorTextBox.Text = "ID: " + a.Id + ", Name: " + a.Name + ", Nationality: " + a.Nationality;
+                        }
+                    }
+                }
+            }
         }
     }
 }
