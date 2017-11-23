61fca4e5497acfaeb8a4a1ef64879dad1f413ade
johannaastrom <johannaastrom@live.se>
2017-11-23 14:15:46 +0100

    Created add button code.

--- SQLLabb1/MainWindow.xaml.cs
+++ SQLLabb1/MainWindow.xaml.cs
@@ -32,7 +32,7 @@ namespace SQLLabb1
             string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Library;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
             using (SqlConnection con = new SqlConnection(connectionString))
             {
-               con.Open();
+                con.Open();
                 var query = "SELECT * FROM Author";
                 using (SqlCommand cmd = new SqlCommand(query, con))
                 {
@@ -81,8 +81,8 @@ namespace SQLLabb1
                             a.Nationality = reader.GetString(2);
 
                             IdTextBox.Text = a.Id.ToString();
-                            AuthorNameTextBox.Text = "Authors name is " + a.Name;
-                            NationalityTextBox.Text = "Author is " + a.Nationality;
+                            AuthorNameTextBox.Text = a.Name;
+                            NationalityTextBox.Text = a.Nationality;
                         }
                     }
                 }
@@ -110,16 +110,16 @@ namespace SQLLabb1
                             b.AuthorID = reader.GetInt32(1);
                             b.Title = reader.GetString(2);
 
-                            BookIdTextBox.Text = "Book has ID number " + b.BookId;
-                            AuthorIdTextBox.Text = "Author has ID number " + b.AuthorID;
-                            TitleTextBox.Text = "The title is " + "''" + b.Title +"''";
+                            BookIdTextBox.Text = b.BookId.ToString();
+                            AuthorIdTextBox.Text = b.AuthorID.ToString();
+                            TitleTextBox.Text = b.Title;
                         }
                     }
                 }
             }
         }
 
-        private void SaveChangesButton_Click(object sender, RoutedEventArgs e)
+        private void SaveChangesButton_Click(object sender, RoutedEventArgs e) 
         {
             #region Gammal kod
             //int index = AuthorListBox.SelectedIndex;
@@ -143,35 +143,112 @@ namespace SQLLabb1
             //    }
             //}
             #endregion
+            
+            #region
+            //string cmdString = "UPDATE Author SET Name = @val2, Nationality = @val3, AuthorId = @val1 WHERE AuhorId=" + IdTextBox.Text;
+            //string connString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Library;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
 
-            string cmdString = "UPDATE Author SET Name = @val2, Nationality = @val3, AuthorId = @val1 WHERE AuhorId=" + IdTextBox.Text;
-            string connString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Library;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            //using (SqlConnection conn = new SqlConnection(connString))
+            //{
+            //    using (SqlCommand comm = new SqlCommand(cmdString, conn))
+            //    {
+            //        comm.Connection = conn;
+            //        comm.CommandText = cmdString;
+            //        // Dispatcher.Invoke(() => AuthorIdTextBox);
+            //        comm.Parameters.AddWithValue("@val1", IdTextBox.Text);
+            //        comm.Parameters.AddWithValue("@val2", AuthorNameTextBox.Text);
+            //        comm.Parameters.AddWithValue("@val3", NationalityTextBox.Text);
 
-            using (SqlConnection conn = new SqlConnection(connString))
+            //        conn.Open();
+            //        comm.ExecuteNonQuery();
+            //    }
+            //}
+            #endregion
+        }
+
+        private void SaveChangesBookButton_Click(object sender, RoutedEventArgs e)
+        {
+            string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Library;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            using (SqlConnection connection = new SqlConnection(connectionString))
             {
-                using (SqlCommand comm = new SqlCommand(cmdString, conn))
+                try
                 {
-                    comm.Connection = conn;
-                    comm.CommandText = cmdString;
-                    // Dispatcher.Invoke(() => AuthorIdTextBox);
-                    comm.Parameters.AddWithValue("@val1", IdTextBox.Text);
-                    comm.Parameters.AddWithValue("@val2", AuthorNameTextBox.Text);
-                    comm.Parameters.AddWithValue("@val3", NationalityTextBox.Text);
+                    connection.Open();
 
-                    conn.Open();
-                    comm.ExecuteNonQuery();
+                    string query = "UPDATE Book SET BookId = '" + BookIdTextBox.Text + "' ," +
+                        " AuthorID = '" + AuthorIdTextBox.Text + "', Title = '" + TitleTextBox.Text +
+                        "' WHERE BookId = '" + BookIdTextBox.Text + "' ";
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.ExecuteNonQuery();
+                    MessageBox.Show("Book has been updated!");
                 }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+
             }
         }
 
-        private void IdTextBox_GotFocus(object sender, RoutedEventArgs e)
+        private void AddAuthorButton_Click(object sender, RoutedEventArgs e)
         {
+            string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Library;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
 
+                    string query = "INSERT INTO Author VALUES AuthorId = '" + IdTextBox.Text + "' ," + //hur skriva denna query?
+                        " Name = '" + AuthorNameTextBox.Text + "', Nationality = '" + NationalityTextBox.Text +
+                        "' WHERE AuthorId = '" + IdTextBox.Text + "' ";
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.ExecuteNonQuery();
+                    MessageBox.Show("A new Author has need created!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+
+            }
         }
 
-        private void AddElementButton_Click(object sender, RoutedEventArgs e)
+        private void AddBookButton_Click(object sender, RoutedEventArgs e)
         {
-            AuthorIdTextBox.Text = string.Empty;
+            string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Library;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                var query = "INSERT INTO Book VALUES BookId = '" + BookIdTextBox.Text + "' ," +
+                        " AuthorID = '" + AuthorIdTextBox.Text + "', Title = '" + TitleTextBox.Text +
+                        "' WHERE BookId = '" + BookIdTextBox.Text + "' ";
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Book b = new Book();
+                            b.BookId = reader.GetInt32(0);
+                            b.AuthorID = reader.GetInt32(1);
+                            b.Title = reader.GetString(2);
+
+                            BookIdTextBox.Text = b.BookId.ToString();
+                            AuthorIdTextBox.Text = b.AuthorID.ToString();
+                            TitleTextBox.Text = b.Title;
+                        }
+                    }
+                }
+            }
         }
     }
 }
